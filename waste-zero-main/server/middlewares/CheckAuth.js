c673a0afdf0cf7ce86d572e70e9165a8ef81909f
Harshitha Reddy <rjharsh2023@gmail.com>
2026-02-19 13:16:12 +0530

    Removed accidentally committed waste-zero-main duplicate project folder

--- waste-zero-main/server/middlewares/CheckAuth.js
+++
@@ -1,9 +0,0 @@
-const checkAuth = (req, res, next) => {
-  if (req.session && req.session.isAuthenticated) {
-    next();
-  } else {
-    res.status(401).json({ message: "Unauthorized" });
-  }
-};
-
-module.exports = checkAuth;
