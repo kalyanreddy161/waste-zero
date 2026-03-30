60d92c61745d4af0bc5b5e503ca563f62d7206b1
kalyanreddy161 <kalyanreddykalyanreddy1611@gmail.com>
2026-03-31 00:31:05 +0530

    Fix secure cookie setting to use string values

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ const sessionMiddleware = session({
   store: store,
     cookie: {
     httpOnly: true,
-    secure: isProduction,          // ✅ only true in production
+    secure: isProduction ? "true" : "false" ,          // ✅ only true in production
     sameSite: isProduction ? "none" : "lax",  // ✅ important
     maxAge: process.env.SESSION_MAX_AGE
       ? parseInt(process.env.SESSION_MAX_AGE, 10)
